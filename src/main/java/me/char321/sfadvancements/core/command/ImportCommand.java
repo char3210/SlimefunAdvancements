c2a0529ee8790b2e3ba7ec188eaa378d6edbc0d3
qwertyuioplkjhgfd <qwertyuioplkjhgfd@users.noreply.github.com>
2022-03-25 10:30:52 -0700

    backups

--- src/main/java/me/char321/sfadvancements/core/command/ImportCommand.java
+++ src/main/java/me/char321/sfadvancements/core/command/ImportCommand.java
@@ -14,6 +14,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -47,11 +50,47 @@ public class ImportCommand implements SubCommand {
             sender.sendMessage(ChatColor.YELLOW + "Any imported advancements may have unknown groups.");
         }
 
+        saveBackups();
         importGroups(pl, groupInputStream);
         importAdvancements(pl, advInputStream);
 
         sender.sendMessage("Done! Restart the server or use /sfa reload to apply the changes.");
         return true;
+    }
+
+    private void saveBackups() {
+        File dataFolder = SFAdvancements.instance().getDataFolder();
+        File backupFolder = new File(dataFolder, "backups");
+        if (!backupFolder.exists()) {
+            backupFolder.mkdirs();
+        }
+        if (!backupFolder.isDirectory()) {
+            throw new IllegalStateException("File " + backupFolder + " is not a directory");
+        }
+
+        File groupFile = new File(dataFolder, "groups.yml");
+        File advFile = new File(dataFolder, "advancements.yml");
+
+        String now = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+        int index = 0;
+        File groupFileOut;
+        File advFileOut;
+        do {
+            index++;
+            groupFileOut = new File(backupFolder, "groups-backup-" + now + " (" + index + ").yml");
+            advFileOut = new File(backupFolder, "advancements-backup-" + now + " (" + index + ").yml");
+        } while (groupFileOut.exists() || advFileOut.exists());
+
+        try {
+            if (groupFile.exists()) {
+                Files.copy(groupFile.toPath(), groupFileOut.toPath());
+            }
+            if (advFile.exists()) {
+                Files.copy(advFile.toPath(), advFileOut.toPath());
+            }
+        } catch (IOException ex) {
+            SFAdvancements.logger().log(Level.SEVERE, ex, () -> "Error while saving advancement backups");
+        }
     }
 
     private void importGroups(Plugin pl, InputStream groupInputStream) {
